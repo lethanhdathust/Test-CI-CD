9dcfed7971fe8c8884a26215cc8f799dc0df8127
LeThanhDat <89300362+lethanhdathust@users.noreply.github.com>
2023-05-10 23:48:51 +0700

    đâ

--- test/index.js
+++ test/index.js
@@ -9,7 +9,7 @@ app.get('/will', function (req, res) {
     res.send('{ "response": "Hello World" }');
 });
 app.get('/ready', function (req, res) {
-    res.send('{ "response": " Great!, It wofdsafrksda!" }');
+    res.send('{ "response": " Great!, It wofdsafrđaksda!" }');
 });
 app.listen(process.env.PORT || 3000);
 module.exports = app;
